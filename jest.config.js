862adcc66134bf266e648b14e5c6c9a2b39a6aa0
Matt Lehrer <lehrerm@gmail.com>
2020-05-03 15:11:36 -0700

    add static testing and update packages

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  watchPlugins: [
+    require.resolve('jest-watch-select-projects'),
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
+};
