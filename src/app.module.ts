9d3afb5f3a6657a03a93fab19ce62d89ba1d7413
Matt Lehrer <lehrerm@gmail.com>
2020-05-04 10:10:32 -0700

    remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
