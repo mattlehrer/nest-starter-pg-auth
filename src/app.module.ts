1a12926a23f4571b80ed4dab81f9e69f14df8580
Matt Lehrer <lehrerm@gmail.com>
2020-05-06 23:56:55 -0700

    use configService for TypeOrmModule and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import config from './config/config';
-import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [config] }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    // TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database') as TypeOrmModuleOptions,
+    }),
     AuthModule,
     UserModule,
   ],
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,17 @@ export default () => ({
   server: {
     port: parseInt(process.env.PORT, 10) || 3000,
   },
+  database: {
+    type: 'postgres',
+    host: process.env.DB_HOSTNAME || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'api',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: Boolean(process.env.TYPEORM_SYNC === 'true') || false,
+    // synchronize: Boolean(process.env.TYPEORM_SYNC === 'true') || true,
+  },
   jwt: {
     expiresIn: process.env.EXPIRES_IN || '1d',
     secret: process.env.JWT_SECRET,
