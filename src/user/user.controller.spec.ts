d6af8d5fdd86aa144f23b48295f81807a194c99e
Matt Lehrer <lehrerm@gmail.com>
2020-05-12 17:05:27 -0700

    improve tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UpdateUserInput } from './dto/update-user.dto';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
+jest.mock('./user.service');
+
+const mockUser = {
+  id: 1,
+  username: 'FAKE_NAME',
+  email: 'F@KE.COM',
+};
+
+const mockReq = {
+  user: mockUser,
+};
+
 describe('User Controller', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [{ provide: UserService, useFactory: () => jest.fn() }],
+      providers: [UserService],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  it('get /me should return current user', async () => {
+    userService.findOneById.mockResolvedValueOnce(mockUser);
+    const response = await userController.getMe(mockReq);
+
+    expect(userService.findOneById).toHaveBeenCalledWith(mockUser.id);
+    expect(userService.findOneById).toHaveBeenCalledTimes(1);
+    expect(response).toEqual(mockUser);
+  });
+
+  it('post /me should update current user and return user with updates', async () => {
+    const updateDto: UpdateUserInput = {
+      email: 'F2@KE.COM',
+    };
+    userService.update.mockResolvedValueOnce({ ...mockUser, ...updateDto });
+    const response = await userController.updateMe(mockReq, updateDto);
+
+    expect(userService.update).toHaveBeenCalledWith(mockUser, updateDto);
+    expect(userService.update).toHaveBeenCalledTimes(1);
+    expect(response).toEqual({ ...mockUser, ...updateDto });
   });
 });
