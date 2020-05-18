c89565724a6020408b946868fa6c2bd7c00c088f
Matt Lehrer <lehrerm@gmail.com>
2020-05-18 00:06:20 -0700

    ignore use before define for functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "no-use-before-define": ["error", { "functions": false }],
+    "@typescript-eslint/no-use-before-define": ["error", { "functions": false }],
   },
 };
